97971e7133756fb5d9a175f98b20c7762aa7e619
Noa <noavandijk@live.nl>
2019-11-19 16:39:31 +0100

    Bar Chart with default values.

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="information">
+  <div id="information">
     <h1>Hier komt een dashboard met grafieken over de gemaakte kilometers</h1>
-  </div>
+
+    <div>
+      <apexchart width="660" type="bar" :options="options" :series="series"></apexchart>
+    </div>
+  </div>  
 </template>
 
 <script>
   export default {
     name: 'Information',
-    data () {
-      return {
-      }
+    data: function() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart'
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr','May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 15, 45, 50, 29, 60, 40, 91, 20, 50, 30, 80]
+      }]
+    }
     }
   }
 </script>
